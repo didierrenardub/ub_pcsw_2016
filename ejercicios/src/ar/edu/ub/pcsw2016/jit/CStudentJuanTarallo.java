68c1441ae33609c8a250c2a141f5c35b8a4f859e
Nachot93 <nachotarallo@gmail.com>
2016-08-08 19:22:26 -0300

    String Name

---
+++ ejercicios/src/ar/edu/ub/pcsw2016/jit/CStudentJuanTarallo.java
@@ -0,0 +1,18 @@
+package ar.edu.ub.pcsw2016.jit;
+
+import ar.edu.ub.pcsw2016.IStudent;
+
+/**
+ * Created by nacho on 8/8/2016.
+ */
+public class CStudentJuanTarallo implements IStudent {
+
+
+    public String name() {
+        return getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
