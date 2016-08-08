31e355ed1fcceea10b47ab8cd977bff8babe447e
Nachot93 <nachotarallo@gmail.com>
2016-08-08 18:48:13 -0300

    Test2, Format code

--- ejercicios/src/ar/edu/ub/pcsw2016/CExercisesMain.java
+++ ejercicios/src/ar/edu/ub/pcsw2016/CExercisesMain.java
@@ -1,8 +1,6 @@
 package ar.edu.ub.pcsw2016;
 
-public class CExercisesMain
-{
-    public static void main(String[] args)
-    {
+public class CExercisesMain {
+    public static void main(String[] args) {
     }
 }
