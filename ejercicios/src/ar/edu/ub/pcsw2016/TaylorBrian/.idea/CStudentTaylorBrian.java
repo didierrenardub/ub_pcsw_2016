983ce23583f3bf808e8e5d8b1452cd78d0eaf873
NibraT <brian.taylor@comunidad.ub.edu.ar>
2016-08-09 02:33:38 -0300

    Progreso
    Pequeño avance en cuanto a los puntos pero con dudas respecto de las
    consignas.

---
+++ ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/.idea/CStudentTaylorBrian.java
@@ -0,0 +1,11 @@
+public class BrianTaylor implements IStudent{
+    this.set name{
+        return "Brian Taylor";
+    }
+    this.set registrationNumbrer{
+        return 03132;
+    }
+    void addExercise(IExercise exercise);
+
+
+}
---
+++ ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/GIT_SETUP.java
@@ -0,0 +1,5 @@
+ class GIT_SETUP implements <IExercise>{
+         void run (){
+             system.out.print.ln("NibraT");
+         }
+        }
