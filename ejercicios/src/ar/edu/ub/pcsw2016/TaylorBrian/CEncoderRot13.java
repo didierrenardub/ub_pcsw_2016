e81b8d70de1fc4f7db80e874c342dec8bdffb990
NibraT <brian.taylor@comunidad.ub.edu.ar>
2016-09-01 07:45:19 -0300

    Modificiación de la clase EncoderRot13
    Se modifico el metodo encode.

--- ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/CEncoderRot13.java
+++ ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/CEncoderRot13.java
@@ -9,16 +9,16 @@ import java.util.ArrayList;
  */
 public class CEncoderRot13 implements IEncoder {
 
-    private char [] a= new char [26] {'a','b','c','d','e','f','g','h','i','j','k','l','m','A','B','C','D','E','F','F',
-            'H','I','J','K','L','M' };
-
-    private char [] n= new char[26] {'n','o','p','q','r','s','t','u','v','w','x','y''z','N','O','P','Q','R','S','T'
-            ,'U','V','W','X','Y','Z'};
-
     public String encode(String what){
+        private char [] a= new char [13] {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
+        private char [] n= new char[13] {'n','o','p','q','r','s','t','u','v','w','x','y''z'};
         if(what  != null){
             String lowerCase= what.toLowerCase();
-            for(int i=0; i<lowerCase.charAt(); i++){
+            for(int i=0; i<lowerCase.length(); i++){
+                String position= lowerCase[i];
+                if(position.contains(a[i]) ){
+                    position.replace());
+                }
 
             }
         }
