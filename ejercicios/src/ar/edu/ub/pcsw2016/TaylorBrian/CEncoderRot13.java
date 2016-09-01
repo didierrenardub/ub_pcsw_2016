bb5523c1391ca7ee99420c423d0d91e1fc3cc1c6
NibraT <brian.taylor@comunidad.ub.edu.ar>
2016-09-01 13:37:06 -0300

    Se modifica el método encode en la clase EndoderRot13
    Se reemplaza el método contains por contentEquals para posibilitar
    comparar un string con un array de chars.
    Ignorar el ultimo return, es erroneo.

--- ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/CEncoderRot13.java
+++ ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/CEncoderRot13.java
@@ -10,16 +10,21 @@ import java.util.ArrayList;
 public class CEncoderRot13 implements IEncoder {
 
     public String encode(String what){
-        private char [] a= new char [13] {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
-        private char [] n= new char[13] {'n','o','p','q','r','s','t','u','v','w','x','y''z'};
+        private char [] a= new char [] {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
+
+        private char [] n= new char[] {'n','o','p','q','r','s','t','u','v','w','x','y''z'};
+
         if(what  != null){
             String lowerCase= what.toLowerCase();
             for(int i=0; i<lowerCase.length(); i++){
                 String position= lowerCase[i];
-                if(position.contains(a[i]) ){
-                    position.replace());
+                if(position.contentEquals(a[i]) ){
+                    position.replace(a[i], n [i]);
                 }
-
+                else(position.contentEquals(n[i])){
+                    position.replace(n[i], a[i]);
+                }
+                return position;
             }
         }
     }
