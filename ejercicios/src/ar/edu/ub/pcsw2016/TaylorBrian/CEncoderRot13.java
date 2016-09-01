2fcc671516b78326352e7290a77ab30946bd9136
NibraT <brian.taylor@comunidad.ub.edu.ar>
2016-09-01 20:44:38 -0300

    Modificaciones metodo del encoder rot13
    Hice unas modificaciones para consultar mañana, no son correctas o no
    esta bien la sintaxis. De no ser correctas los cambios seran revertidos.

--- ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/CEncoderRot13.java
+++ ejercicios/src/ar/edu/ub/pcsw2016/TaylorBrian/CEncoderRot13.java
@@ -10,22 +10,24 @@ import java.util.ArrayList;
 public class CEncoderRot13 implements IEncoder {
 
     public String encode(String what){
-        private char [] a= new char [] {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
+        char [] a= new char [] {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
 
-        private char [] n= new char[] {'n','o','p','q','r','s','t','u','v','w','x','y''z'};
+        char [] n= new char[] {'n','o','p','q','r','s','t','u','v','w','x','y''z'};
 
         if(what  != null){
             String lowerCase= what.toLowerCase();
+            String encodedPhrase= " ";
             for(int i=0; i<lowerCase.length(); i++){
-                String position= lowerCase[i];
-                if(position.contentEquals(a[i]) ){
-                    position.replace(a[i], n [i]);
+                String position = lowerCase[i];
+                if(position.contains(char [] a)){
+                    encodedPhrase= position.replace(position[i], n[i]);
                 }
                 else(position.contentEquals(n[i])){
-                    position.replace(n[i], a[i]);
+                        encodedPhrase=position.replace(n[i], a[i]);
                 }
-                return position;
+                return encodedPhrase;
             }
+        return null;
         }
     }
 }
