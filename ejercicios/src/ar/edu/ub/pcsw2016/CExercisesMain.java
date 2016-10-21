c1163cc5b75b3174da1a29018ba795057eeac87c
Didier Renard <didier.renard@comunidad.ub.edu.ar>
2016-10-21 17:37:47 -0300

    - Mejorada la aplicación

--- ejercicios/src/ar/edu/ub/pcsw2016/CExercisesMain.java
+++ ejercicios/src/ar/edu/ub/pcsw2016/CExercisesMain.java
@@ -5,40 +5,108 @@ import ar.edu.ub.pcsw2016.diegovega.RPSDiego;
 import ar.edu.ub.pcsw2016.fginzo.CExerciseEncoding;
 import ar.edu.ub.pcsw2016.fginzo.CJuegoRPS;
 
+import java.util.Scanner;
+
 public class CExercisesMain
 {
-    public static void main(String[] args)
+    private CExercisesMain()
     {
-        CStudentStatistics stats = new CStudentStatistics();
-        stats.runExercises();
-
-        CRPSTournament t = new CRPSTournament();
-        CRPSBattleBot bb = new CRPSBattleBot("UB");
-        t.addPlayer(new CRPSPlayer(bb, bb));
-        t.addPlayer(new CRPSPlayer(stats.student("Diego Santiago Vega"), new RPSDiego()));
-        t.addPlayer(new CRPSPlayer(stats.student("Didier Renard"), new CRPSIADidier()));
-        t.addPlayer(new CRPSPlayer(stats.student("Francisco Ginzo"), new CJuegoRPS()));
-
-        t.play();
-
-        /*IStudent diego = stats.student("Diego Santiago Vega");
+        this.m_stats = new CStudentStatistics();
+    }
+    
+    private int userInput(int min, int max)
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int input = min - 1;
+        
+        while(input < min || input > max)
+        {
+            try
+            {
+                input = s.nextInt();
+            }
+            catch(Exception e)
+            {
+                s.nextLine();
+            }
+            finally
+            {
+                
+                if(input < min || input > max)
+                {
+                    System.out.println("Invalid input, try again:");
+                }
+            }
+        }
+        
+        return input;
+    }
+    
+    private CStudentStatistics stats()
+    {
+        return m_stats;
+    }
+    
+    private void studentsGrading()
+    {
+        IStudent diego = stats().student("Diego Santiago Vega");
         diego.exercise(IExercise.ID.GIT_SETUP).setGrade(CGrader.instance().completion(10.0f).finalGrade());
         diego.exercise(IExercise.ID.STUDENT_CREATION).setGrade(CGrader.instance().completion(8.0f).prolixity(8.0f).reliability(8.0f).design(5.0f).finalGrade());
         diego.exercise(IExercise.ID.ENCODING).setGrade(CGrader.instance().completion(10.0f).prolixity(7.0f).reliability(10.0f).design(10.0f).finalGrade());
-        
-        IStudent francisco = stats.student("Francisco Ginzo");
+    
+        IStudent francisco = stats().student("Francisco Ginzo");
         francisco.exercise(IExercise.ID.GIT_SETUP).setGrade(CGrader.instance().completion(10.0f).finalGrade());
         francisco.exercise(IExercise.ID.STUDENT_CREATION).setGrade(CGrader.instance().completion(10.0f).prolixity(9.5f).reliability(9.0f).design(5.0f).finalGrade());
         francisco.exercise(IExercise.ID.ENCODING).setGrade(CGrader.instance().completion(10.0f).prolixity(9.5f).reliability(9.0f).design(10.0f).finalGrade());
     
-        IStudent juanIgnacio = stats.student("Juan Tarallo");
+        IStudent juanIgnacio = stats().student("Juan Tarallo");
         juanIgnacio.exercise(IExercise.ID.GIT_SETUP).setGrade(CGrader.instance().completion(10.0f).finalGrade());
         juanIgnacio.exercise(IExercise.ID.STUDENT_CREATION).setGrade(CGrader.instance().completion(1.0f).prolixity(9.0f).reliability(2.0f).design(2.0f).finalGrade());
     
         System.out.println("Average grades:");
-        for(IStudent s : stats.students())
+        for(IStudent s : stats().students())
         {
             System.out.println("\t" + s.name() + ": " + s.gradesAverage());
-        }*/
+        }
     }
+    
+    private void rockPaperScissorsTournament()
+    {
+        CRPSTournament t = new CRPSTournament();
+        CRPSBattleBot bb = new CRPSBattleBot("UB");
+        t.addPlayer(new CRPSPlayer(bb, bb));
+        t.addPlayer(new CRPSPlayer(stats().student("Diego Santiago Vega"), new RPSDiego()));
+        t.addPlayer(new CRPSPlayer(stats().student("Didier Renard"), new CRPSIADidier()));
+        t.addPlayer(new CRPSPlayer(stats().student("Francisco Ginzo"), new CJuegoRPS()));
+        t.play();
+    }
+        
+    public static void main(String[] args)
+    {
+        CExercisesMain app = new CExercisesMain();
+        
+        System.out.println("Choose an action:\n1 - Rock, Paper, Scissors tournament\n2 - Console Warriors tournament\n3 - Run exercises\n4 - Students grading");
+        
+        switch(app.userInput(1, 4))
+        {
+            case 1:
+                app.rockPaperScissorsTournament();
+            break;
+            
+            case 2:
+                //
+            break;
+            
+            case 3:
+                app.stats().runExercises();
+            break;
+            
+            case 4:
+                app.studentsGrading();
+            break;
+        }
+    }
+    
+    private CStudentStatistics m_stats;
 }
